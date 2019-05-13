c9b7d71fb883befbf5f54b9e890bf69cce64399c
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-05-13 12:39:57 -0700

    added in main layout for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,6 +16,30 @@ class Home extends Component {
                     <div className="content-type-3">3</div>
                 </div>
             </div>
+
+                        <div className="main-home">
+                <div className="temp-placeholder">Main Content</div>
+            </div>
+            <div className="bot-home-div">
+                <h3 className="temp-h3-title">Product Placeholder</h3>
+                <div className="bot-cat-home-div">
+                    <div className="content-type-1">4</div>
+                    <div className="content-type-2">5</div>
+                    <div className="content-type-3">6</div>
+                </div>
+            </div>
+
+                        <div className="main-home">
+                <div className="temp-placeholder">Main Content</div>
+            </div>
+            <div className="bot-home-div">
+                <h3 className="temp-h3-title">Product Placeholder</h3>
+                <div className="bot-cat-home-div">
+                    <div className="content-type-1">7</div>
+                    <div className="content-type-2">8</div>
+                    <div className="content-type-3">9</div>
+                </div>
+            </div>
         </div>    
     );
   }
