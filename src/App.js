930c9a7401cfbecf4cee884b5b6416397a16cc99
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-05-12 18:36:13 -0700

    added in video placeholder and home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div>
         <h1>Off White</h1>
+      </div>
       </header>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="main-home">
+        <div className="temp-placeholder">Video Placeholder</div>
+      </div>
+    );
+  }
+} 
+
+export default Home;
