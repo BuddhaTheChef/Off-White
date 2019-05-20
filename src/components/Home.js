7201a947cf85de5c1a378ca78652ace4ab11f8aa
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-05-20 13:56:17 -0700

    added in bonsai trees

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,8 @@ class Home extends Component {
         playerVars: { // https://developers.google.com/youtube/player_parameters
           autoplay: 1,
           controls: 0,
-          start: 0,
+          start: 60,
+          loop: 1,
           mute:1,
           enablejsapi: 1,
           modestbranding: 0,
@@ -45,9 +46,9 @@ class Home extends Component {
             <div className="bot-home-div-1">
                 <h3 className="temp-h3-title">Product Placeholder</h3>
                 <div className="bot-cat-home-div">
-                    <div className="content-type-1">1</div>
-                    <div className="content-type-2">2</div>
-                    <div className="content-type-3">3</div>
+                    <div className="content-type-1"><img className="pics1" src="https://images.homedepot-static.com/productImages/05076193-7591-472e-b0c5-5530252e4806/svn/brussel-s-bonsai-bonsai-trees-dt-7079gmj-64_1000.jpg" alt="wow"></img></div>
+                    <div className="content-type-2"><img className="pics1" src="https://images.homedepot-static.com/productImages/bd8393eb-9cbd-4224-b7b8-7e15fbef5f7e/svn/brussel-s-bonsai-bonsai-trees-ct-0116ft-64_1000.jpg" alt="tree"></img></div>
+                    <div className="content-type-3"><img className="pics1" src="https://images-na.ssl-images-amazon.com/images/I/81Rx03JgEDL._SX355_.jpg" alt="wow"></img></div>
                 </div>
             </div>
 
@@ -57,9 +58,9 @@ class Home extends Component {
             <div className="bot-home-div-2">
                 <h3 className="temp-h3-title">Product Placeholder</h3>
                 <div className="bot-cat-home-div">
-                    <div className="content-type-1">4</div>
-                    <div className="content-type-2">5</div>
-                    <div className="content-type-3">6</div>
+                    <div className="content-type-1"><img className="pics1" src="https://cdn11.bigcommerce.com/s-bhqinueo9m/images/stencil/1024x1024/products/14642/8047/ginseng-grafted-ficus-small-indoor-bonsai-ct7012gmf__47740.1480631167.jpg?c=2" alt="wow"></img></div>
+                    <div className="content-type-2"><img className="pics1" src="https://cdn1.bigcommerce.com/server4100/6ys4nr/products/1757/images/5932/ChineseElm__34952.1417016213.1280.1280.JPG?c=2" alt="wow"></img></div>
+                    <div className="content-type-3"><img className="pics1" src="https://cdn11.bigcommerce.com/s-bhqinueo9m/images/stencil/1024x1024/products/14633/8040/fukien-tea-medium-indoor-bonsai-ct0116ft__99376.1480631164.jpg?c=2" alt="wow"></img></div>
                 </div>
             </div>
 
@@ -69,9 +70,9 @@ class Home extends Component {
             <div className="bot-home-div-3">
                 <h3 className="temp-h3-title">Product Placeholder</h3>
                 <div className="bot-cat-home-div">
-                    <div className="content-type-1">7</div>
-                    <div className="content-type-2">8</div>
-                    <div className="content-type-3">9</div>
+                    <div className="content-type-1"><img className="pics1" src="https://www.easternleaf.com/v/vspfiles/photos/804550-03-2.jpg" alt="wow"></img></div>
+                    <div className="content-type-2"><img className="pics1" src="https://www.redlandsdailyfacts.com/wp-content/uploads/migration/2015/201505/NEWS_150519634_AR_0_TGUQUXVHUNFI.jpg?w=535" alt="wow"></img></div>
+                    <div className="content-type-3"><img className="pics1" src="https://cdn.shopify.com/s/files/1/0422/3365/collections/Fukien_Bonsai_Tree_large_68344548-0145-41b4-8409-2a886f17d724_large.jpg?v=1502917813" alt="wow"></img></div>
                 </div>
             </div>
         
