e2ccff6848763a9095a678efa3d7bc5fb41faade
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-05-23 17:08:02 -0700

    added in modals dynamically

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,27 +11,84 @@ class Home extends Component {
       showModal: false,
       showModal2: false
     };
-
+    //Open Modals
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleOpenModal2 = this.handleOpenModal2.bind(this);
+    this.handleOpenModal3 = this.handleOpenModal3.bind(this);
+    this.handleOpenModal4 = this.handleOpenModal4.bind(this);
+    this.handleOpenModal5 = this.handleOpenModal5.bind(this);
+    this.handleOpenModal6 = this.handleOpenModal6.bind(this);
+    this.handleOpenModal7 = this.handleOpenModal7.bind(this);
+    this.handleOpenModal8 = this.handleOpenModal8.bind(this);
+    this.handleOpenModal9 = this.handleOpenModal9.bind(this);
+    //Close Modals
     this.handleCloseModal = this.handleCloseModal.bind(this);
     this.handleCloseModal2 = this.handleCloseModal2.bind(this);
+    this.handleCloseModal3 = this.handleCloseModal3.bind(this);
+    this.handleCloseModal4 = this.handleCloseModal4.bind(this);
+    this.handleCloseModal5 = this.handleCloseModal5.bind(this);
+    this.handleCloseModal6 = this.handleCloseModal6.bind(this);
+    this.handleCloseModal7 = this.handleCloseModal7.bind(this);
+    this.handleCloseModal8 = this.handleCloseModal8.bind(this);
+    this.handleCloseModal9 = this.handleCloseModal9.bind(this);
+   
   }
 
   handleOpenModal() {
     this.setState({ showModal: true });
   }
-
   handleOpenModal2() {
     this.setState({ showModal2: true });
   }
+  handleOpenModal3() {
+    this.setState({ showModal3: true });
+  }
+  handleOpenModal4() {
+    this.setState({ showModal4: true });
+  }
+  handleOpenModal5() {
+    this.setState({ showModal5: true });
+  }
+  handleOpenModal6() {
+    this.setState({ showModal6: true });
+  }
+  handleOpenModal7() {
+    this.setState({ showModal7: true });
+  }
+  handleOpenModal8() {
+    this.setState({ showModal8: true });
+  }
+  handleOpenModal9() {
+    this.setState({ showModal9: true });
+  }
+
 
   handleCloseModal() {
     this.setState({ showModal: false });
   }
-
   handleCloseModal2() {
     this.setState({ showModal2: false });
+  }
+  handleCloseModal3() {
+    this.setState({ showModal3: false });
+  }
+  handleCloseModal4() {
+    this.setState({ showModal4: false });
+  }
+  handleCloseModal5() {
+    this.setState({ showModal5: false });
+  }
+  handleCloseModal6() {
+    this.setState({ showModal6: false });
+  }
+  handleCloseModal7() {
+    this.setState({ showModal7: false });
+  }
+  handleCloseModal8() {
+    this.setState({ showModal8: false });
+  }
+  handleCloseModal9() {
+    this.setState({ showModal9: false });
   }
 
   render() {
@@ -93,7 +150,7 @@ class Home extends Component {
               />
               Chinese Elm
             </div>
-            <div className="content-type-3">
+            <div className="content-type-3" onClick={this.handleOpenModal3}>
               <img
                 className="pics1"
                 src="https://images-na.ssl-images-amazon.com/images/I/81Rx03JgEDL._SX355_.jpg"
@@ -134,7 +191,7 @@ class Home extends Component {
         <div className="bot-home-div-2">
           <h3 className="temp-h3-title">Intermdeiate Trees</h3>
           <div className="bot-cat-home-div">
-            <div className="content-type-1">
+            <div className="content-type-1" onClick={this.handleOpenModal4}>
               <img
                 className="pics1"
                 src="https://cdn11.bigcommerce.com/s-bhqinueo9m/images/stencil/1024x1024/products/14642/8047/ginseng-grafted-ficus-small-indoor-bonsai-ct7012gmf__47740.1480631167.jpg?c=2"
@@ -142,7 +199,7 @@ class Home extends Component {
               />
               Shohin Flowering Tea Tree
             </div>
-            <div className="content-type-2">
+            <div className="content-type-2" onClick={this.handleOpenModal5}>
               <img
                 className="pics1"
                 src="https://cdn1.bigcommerce.com/server4100/6ys4nr/products/1757/images/5932/ChineseElm__34952.1417016213.1280.1280.JPG?c=2"
@@ -150,7 +207,7 @@ class Home extends Component {
               />
               Golden Gate Ficus
             </div>
-            <div className="content-type-3">
+            <div className="content-type-3" onClick={this.handleOpenModal6}>
               <img
                 className="pics1"
                 src="https://cdn11.bigcommerce.com/s-bhqinueo9m/images/stencil/1024x1024/products/14633/8040/fukien-tea-medium-indoor-bonsai-ct0116ft__99376.1480631164.jpg?c=2"
@@ -197,7 +254,7 @@ class Home extends Component {
         <div className="bot-home-div-3">
           <h3 className="temp-h3-title">Advanced Trees</h3>
           <div className="bot-cat-home-div">
-            <div className="content-type-1">
+            <div className="content-type-1" onClick={this.handleOpenModal7}>
               <img
                 className="pics1"
                 src="https://www.easternleaf.com/v/vspfiles/photos/804550-03-2.jpg"
@@ -205,7 +262,7 @@ class Home extends Component {
               />
               Minature Boxwood
             </div>
-            <div className="content-type-2">
+            <div className="content-type-2" onClick={this.handleOpenModal8}>
               <img
                 className="pics1"
                 src="https://www.redlandsdailyfacts.com/wp-content/uploads/migration/2015/201505/NEWS_150519634_AR_0_TGUQUXVHUNFI.jpg?w=535"
@@ -213,7 +270,7 @@ class Home extends Component {
               />
               Juniper procubens nana
             </div>
-            <div className="content-type-3">
+            <div className="content-type-3" onClick={this.handleOpenModal9}>
               <img
                 className="pics1"
                 src="https://cdn.shopify.com/s/files/1/0422/3365/collections/Fukien_Bonsai_Tree_large_68344548-0145-41b4-8409-2a886f17d724_large.jpg?v=1502917813"
@@ -280,6 +337,69 @@ class Home extends Component {
         >
           <p>Modal text!</p>
           <button onClick={this.handleCloseModal}>Close Modal</button>
+        </Modal>
+
+        <Modal
+          isOpen={this.state.showModal2}
+          contentLabel="Modal #2 Global Style Override Example"
+          onRequestClose={this.handleCloseModal2}
+        >
+          <p>Modal #2 text!</p>
+          <button onClick={this.handleCloseModal2}>Close Modal</button>
+        </Modal>
+
+        <Modal
+          isOpen={this.state.showModal3}
+          contentLabel="Modal #3 Global Style Override Example"
+          onRequestClose={this.handleCloseModal3}
+        >
+          <p>Modal #3 text!</p>
+          <button onClick={this.handleCloseModal3}>Close Modal</button>
+        </Modal>
+
+        <Modal
+          isOpen={this.state.showModal4}
+          contentLabel="Modal #4 Global Style Override Example"
+          onRequestClose={this.handleCloseModal4}
+        >
+          <p>Modal #4 text!</p>
+          <button onClick={this.handleCloseModal4}>Close Modal</button>
+        </Modal>
+
+        <Modal
+          isOpen={this.state.showModal2}
+          contentLabel="Modal #2 Global Style Override Example"
+          onRequestClose={this.handleCloseModal2}
+        >
+          <p>Modal #2 text!</p>
+          <button onClick={this.handleCloseModal2}>Close Modal</button>
+        </Modal>
+
+        <Modal
+          isOpen={this.state.showModal2}
+          contentLabel="Modal #2 Global Style Override Example"
+          onRequestClose={this.handleCloseModal2}
+        >
+          <p>Modal #2 text!</p>
+          <button onClick={this.handleCloseModal2}>Close Modal</button>
+        </Modal>
+
+        <Modal
+          isOpen={this.state.showModal2}
+          contentLabel="Modal #2 Global Style Override Example"
+          onRequestClose={this.handleCloseModal2}
+        >
+          <p>Modal #2 text!</p>
+          <button onClick={this.handleCloseModal2}>Close Modal</button>
+        </Modal>
+
+        <Modal
+          isOpen={this.state.showModal2}
+          contentLabel="Modal #2 Global Style Override Example"
+          onRequestClose={this.handleCloseModal2}
+        >
+          <p>Modal #2 text!</p>
+          <button onClick={this.handleCloseModal2}>Close Modal</button>
         </Modal>
 
         <Modal
